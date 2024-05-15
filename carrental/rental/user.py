630fdbe086dce94e705958fe3a86b9d55d18b02a
DanielCachro <danielcachro@gmail.com>
2024-05-15 15:35:21 +0200

    15.05.2024 DC

--- carrental/rental/user.py
+++ carrental/rental/user.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.forms import inlineformset_factory
-from . import models
+from . import forms
 
 def login(request):
     return render(request, 'login.html.jinja')
@@ -10,5 +8,12 @@ def logout(request):
     return render(request, 'logout.html.jinja')
 
 def register(request):
-    form = inlineformset_factory(models.RegistrationForm, models.AddressForm)
-    return render(request, 'register.html.jinja', {'form': form})
+    if request.method == 'GET':
+        form = forms.RegistrationForm()
+        form_address = forms.UserAddressFormSet()
+        return render(request, 'register.html.jinja', {'form': form, 'form_address': form_address})
+    elif request.method == 'POST':
+        form = forms.RegistrationForm(request.POST)
+        return render(request, 'register.html.jinja', {'message': 'Success'})
+    else:
+        return render(request, 'register.html.jinja')
